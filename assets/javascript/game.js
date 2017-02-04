92bd59af64e486bc8e57f1e3302ec68fed2775a3
Ralph Manlapig <Ralph Manlapig>
2017-02-04 10:19:41 -0500

    updated json

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
 
 var random = Math.floor(Math.random() * 25);
 console.log(random);
